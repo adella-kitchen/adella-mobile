9822f36967e18f89103d432bdc42ee1e3425d13f
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-03-25 20:16:36 +0700

    Update myColors.dart

--- lib/theme/myColors.dart
+++ lib/theme/myColors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class myColors {
   Color primaryColor = const Color(0xFFBE3144);
   Color grey = const Color(0xFFB0B0B0);
+  Color black = Color(0xFF292b2c);
 }
