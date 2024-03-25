cb4cfad3c14597ec1db9271a2566261f72fab3c0
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-03-25 17:24:37 +0700

    import custom color and theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Kelas untuk mengatur tema aplikasi
+class AppTheme {
+  // Fungsi untuk mengembalikan tema aplikasi
+  static ThemeData getAppTheme() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      useMaterial3: true,
+      // Konfigurasi font default
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(fontFamily: 'Poppins'), 
+        bodyLarge:
+            TextStyle(fontFamily: 'Poppins'), 
+        bodyMedium: TextStyle(
+            fontFamily:
+                'Poppins'), 
+      ),
+    );
+  }
+}
---
+++ lib/theme/myColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class myColors {
+  Color primaryColor = const Color(0xFFBE3144);
+  Color grey = const Color(0xFFB0B0B0);
+}
