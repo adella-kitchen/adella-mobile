5ddd9ef8727f50ab4bc8fad40d80a029f8127946
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-04-19 19:48:47 +0700

    change to getx format

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,15 @@
-import 'package:adella_kitchen/components/partials/navbar.dart';
-import 'package:adella_kitchen/view/home.dart';
-import 'package:adella_kitchen/view/login.dart';
-import 'package:adella_kitchen/view/register.dart';
-import 'package:adella_kitchen/view/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:adella_kitchen/theme/app_theme.dart';
-import 'package:flutter/services.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.dark));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Adella Kitchen',
-      theme: AppTheme.getAppTheme(),
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => const Login(),
-        '/register': (context) => const Register(),
-        '/home': (context) => const Home(),
-        '/nav': (context) => const Navbar(),
-      },
-      initialRoute: '/',
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
--- lib/theme/myColors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyColors {
-  Color primaryColor = const Color(0xFFBE3144);
-  Color grey = const Color(0xFFB0B0B0);
-  Color black = Color(0xFF292b2c);
-}
