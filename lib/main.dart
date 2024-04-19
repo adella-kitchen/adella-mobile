b75e3ad63858e58853a8578d69a894df6a203de6
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-04-19 20:24:02 +0700

    update main to remote debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adella_kitchen/app/modules/login/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -7,6 +8,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
