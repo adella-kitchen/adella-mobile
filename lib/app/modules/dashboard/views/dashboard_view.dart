54e3a48f9a14cd54978d318c90575991f64137fb
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-04-19 20:22:34 +0700

    create navbar

---
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -0,0 +1,67 @@
+import 'package:adella_kitchen/app/modules/cart/views/cart_view.dart';
+import 'package:adella_kitchen/app/modules/explore/views/explore_view.dart';
+import 'package:adella_kitchen/app/modules/home/views/home_view.dart';
+import 'package:adella_kitchen/app/modules/profile/views/profile_view.dart';
+import 'package:adella_kitchen/theme/color.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+
+import '../controllers/dashboard_controller.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<DashboardController>(builder: (controller) {
+      return Scaffold(
+        body: SafeArea(
+            child: IndexedStack(
+          index: controller.tabIndex,
+          children: const [
+            HomeView(),
+            ExploreView(),
+            CartView(),
+            ProfileView()
+          ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: myColor().primaryColor,
+          onTap: controller.changeTabIndex,
+          currentIndex: controller.tabIndex,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _bottomNavigationBarItem(
+                activeIcon: Ionicons.home,
+                icon: Ionicons.home_outline,
+                label: 'Home'),
+            _bottomNavigationBarItem(
+                activeIcon: Ionicons.search,
+                icon: Ionicons.search_outline,
+                label: 'Search'),
+            _bottomNavigationBarItem(
+                activeIcon: Ionicons.cart,
+                icon: Ionicons.cart_outline,
+                label: 'Cart'),
+            _bottomNavigationBarItem(
+                activeIcon: Ionicons.person,
+                icon: Ionicons.person_outline,
+                label: 'Profile'),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+_bottomNavigationBarItem(
+    {required IconData activeIcon,
+    required IconData icon,
+    required String label}) {
+  return BottomNavigationBarItem(
+      activeIcon: Icon(activeIcon), icon: Icon(icon), label: label);
+}
