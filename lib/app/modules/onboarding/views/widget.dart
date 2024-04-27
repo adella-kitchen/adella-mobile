f7a9f02287a0084761260b1aae1148e39802ca9d
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-04-27 18:37:49 +0700

    create onboarding page widget

--- lib/app/modules/onboarding/views/widget.dart
+++ lib/app/modules/onboarding/views/widget.dart
@@ -1,3 +1,10 @@
+import 'package:adella_kitchen/app/modules/home/views/widget.dart';
+import 'package:adella_kitchen/app/modules/onboarding/controllers/onboarding_controller.dart';
+import 'package:adella_kitchen/theme/color.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
 class skipBtn extends StatelessWidget {
   const skipBtn({
     super.key,
@@ -14,5 +21,44 @@ class skipBtn extends StatelessWidget {
               'Lewati',
               style: TextStyle(color: myColor().primaryColor),
             )));
+  }
+}
+
+class onBoardingPage extends StatelessWidget {
+  const onBoardingPage({
+    super.key,
+    required this.title,
+    required this.desc,
+    required this.imageUrl,
+  });
+
+  final String title, desc, imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(width: 250, height: 250, image: AssetImage(imageUrl)),
+          const SizedBox(
+            height: 20,
+          ),
+          titleText(
+            text: title,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            desc,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: myColor().grey500),
+          )
+        ],
+      ),
+    );
   }
 }
