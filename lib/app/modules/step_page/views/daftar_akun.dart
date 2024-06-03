4844e9458c0858238b99123e1c8c2e1fd6bdf323
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2024-06-03 19:51:07 +0700

    create step page

--- lib/app/modules/step_page/views/daftar_akun.dart
+++ lib/app/modules/step_page/views/daftar_akun.dart
@@ -1,10 +1,16 @@
 import 'package:adella_kitchen/app/modules/step_page/controllers/step_page_controller.dart';
 import 'package:adella_kitchen/theme/color.dart';
-import 'package:adella_kitchen/theme/widget/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class DaftarAkun extends StatelessWidget {
+class DaftarAkun extends StatefulWidget {
+  DaftarAkun({Key? key}) : super(key: key);
+
+  @override
+  _DaftarAkunState createState() => _DaftarAkunState();
+}
+
+class _DaftarAkunState extends State<DaftarAkun> {
   final StepPageController stepPageController = Get.find<StepPageController>();
 
   // Controllers for each TextField
@@ -14,146 +20,264 @@ class DaftarAkun extends StatelessWidget {
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
-  DaftarAkun({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Method untuk mereset showError dan errorMessage
+  void resetError() {
+    setState(() {
+      showErrorNama = false;
+      showErrorEmail = false;
+      showErrorPassword = false;
+      showErrorConfirmPassword = false;
+    });
+  }
+
+  bool showErrorNama = false;
+  bool showErrorEmail = false;
+  bool showErrorPassword = false;
+  bool showErrorConfirmPassword = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Buat Akun',
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Buat Akun',
               style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: Colors.black),
-              textAlign: TextAlign.left),
-
-          SizedBox(height: 5), // Optional: Space between text elements
-          Text(
-            'Masukkan data diri dan daftarkan akunmu untuk menikmati fitur lengkap dari Adella Kitchen',
-            style: TextStyle(fontSize: 15),
-            textAlign: TextAlign.left,
-          ),
-          SizedBox(height: 35), // Optional: Space between text and TextField
-          Container(
-            width: 350,
-            height: 40,
-            child: TextField(
-              controller: namaController,
-              decoration: InputDecoration(
-                labelText: 'Nama Lengkap Kamu',
-                labelStyle: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black), // Custom label text size
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(5),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Masukkan data diri dan daftarkan akunmu untuk menikmati fitur lengkap dari Adella Kitchen',
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 35),
+            Container(
+              width: 350,
+              child: TextFormField(
+                controller: namaController,
+                decoration: InputDecoration(
+                  labelText: 'Nama Lengkap Kamu',
+                  labelStyle: TextStyle(fontSize: 12, color: Colors.black),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  prefixIcon: Icon(Icons.person),
+                  isDense: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 10),
                 ),
-                prefixIcon: Icon(Icons.person),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    setState(() {
+                      showErrorNama = true;
+                    });
+                    return 'Nama lengkap tidak boleh kosong';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  resetError(); // Panggil method resetError saat teks diubah
+                },
               ),
             ),
-          ),
-          SizedBox(
-              height:
-                  20), // Optional: Space between TextField and new TextField
-          Container(
-            width: 350,
-            height: 40,
-            child: TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Alamat Email Kamu',
-                labelStyle: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black), // Custom label text size
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(5),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              child: TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Alamat Email Kamu',
+                  labelStyle: TextStyle(fontSize: 12, color: Colors.black),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                  isDense: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 10),
                 ),
-                prefixIcon: Icon(Icons.email),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    setState(() {
+                      showErrorEmail = true;
+                    });
+                    return 'Email tidak boleh kosong';
+                  }
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    setState(() {
+                      showErrorEmail = true;
+                    });
+                    return 'Masukkan email yang valid';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  resetError(); // Panggil method resetError saat teks diubah
+                },
               ),
             ),
-          ),
-          SizedBox(
-              height:
-                  20), // Optional: Space between TextField and new TextField
-          Container(
-            width: 350,
-            height: 40,
-            child: TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Kata Sandi',
-                labelStyle: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black), // Custom label text size
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(5),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              child: TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Kata Sandi',
+                  labelStyle: TextStyle(fontSize: 12, color: Colors.black),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  isDense: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 10),
                 ),
-                prefixIcon: Icon(Icons.lock),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    setState(() {
+                      showErrorPassword = true;
+                    });
+                    return 'Kata sandi tidak boleh kosong';
+                  }
+                  if (value.length < 6) {
+                    setState(() {
+                      showErrorPassword = true;
+                    });
+                    return 'Kata sandi harus minimal 6 karakter';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  resetError(); // Panggil method resetError saat teks diubah
+                },
               ),
-              obscureText: true, // To hide the password text
             ),
-          ),
-          SizedBox(
-              height:
-                  20), // Optional: Space between TextField and new TextField
-          Container(
-            width: 350,
-            height: 40,
-            child: TextField(
-              controller: confirmPasswordController,
-              decoration: InputDecoration(
-                labelText: 'Konfirmasi Kata Sandi',
-                labelStyle: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black), // Custom label text size
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(5),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              child: TextFormField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: 'Konfirmasi Kata Sandi',
+                  labelStyle: TextStyle(fontSize: 12, color: Colors.black),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  isDense: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 10),
                 ),
-                prefixIcon: Icon(Icons.lock),
-              ),
-              obscureText: true, // To hide the password text
-            ),
-          ),
-          SizedBox(height: 45),
-          ElevatedButton(
-            onPressed: () {
-              // Check if any field is empty
-              if (namaController.text.isEmpty ||
-                  emailController.text.isEmpty ||
-                  passwordController.text.isEmpty ||
-                  confirmPasswordController.text.isEmpty) {
-                CustomSnackBar.showWarning(
-                    'Peringatan', 'Semua kolom harus diisi');
-              } else {
-                stepPageController.nextStep();
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: myColor().primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              fixedSize: Size(350, 40),
-            ),
-            child: Text(
-              'Daftar',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    setState(() {
+                      showErrorConfirmPassword = true;
+                    });
+                    return 'Konfirmasi kata sandi tidak boleh kosong';
+                  }
+                  if (value != passwordController.text) {
+                    setState(() {
+                      showErrorConfirmPassword = true;
+                    });
+                    return 'Kata sandi tidak cocok';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  resetError(); // Panggil method resetError saat teks diubah
+                },
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 45),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  stepPageController.nextStep();
+                } else {
+                  // Set showError berdasarkan kondisi validasi
+                  setState(() {
+                    showErrorNama = namaController.text.isEmpty;
+                    showErrorEmail = emailController.text.isEmpty ||
+                        !RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                            .hasMatch(emailController.text);
+                    showErrorPassword = passwordController.text.isEmpty ||
+                        passwordController.text.length < 6;
+                    showErrorConfirmPassword =
+                        confirmPasswordController.text.isEmpty ||
+                            confirmPasswordController.text !=
+                                passwordController.text;
+                  });
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: myColor().primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                fixedSize: Size(350, 40),
+              ),
+              child: Text(
+                'Daftar',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
