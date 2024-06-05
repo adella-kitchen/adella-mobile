b3c834c28407357f4c7ceb4ff8e40dce00087c22
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2024-06-05 08:56:18 +0700

    update view

--- lib/app/modules/detail_produk/views/detail_produk_view.dart
+++ lib/app/modules/detail_produk/views/detail_produk_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/detail_produk_controller.dart';
 
 class DetailProdukView extends GetView<DetailProdukController> {
@@ -11,13 +9,45 @@ class DetailProdukView extends GetView<DetailProdukController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DetailProdukView'),
+        title: const Text('Detail Produk'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Id menu ${controller.idMenu}',
-          style: const TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.asset(
+                '/img/menu1.png',
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Pake Catering Ayam',
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Rp. 10.000',
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            const SizedBox(height: 8),
+          ],
         ),
       ),
     );
