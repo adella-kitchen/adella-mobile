eb76775dd59715000f7f6c98ff44ffca36422776
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-06-10 14:59:33 +0700

    Update product.dart

--- lib/app/data/models/product.dart
+++ lib/app/data/models/product.dart
@@ -14,6 +14,28 @@ class AllMenu {
     required this.priceMenu,
     required this.imgUrl,
   });
+
+  factory AllMenu.fromJson(Map<String, dynamic> json) {
+    return AllMenu(
+      idMenu: json['id_menu'],
+      menuCategory: json['menu_category'],
+      menuName: json['menu_name'],
+      descMenu: json['desc_menu'],
+      priceMenu: json['price_menu'],
+      imgUrl: json['menu_img'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id_menu': idMenu,
+      'menu_category': menuCategory,
+      'menu_name': menuName,
+      'desc_menu': descMenu,
+      'price_menu': priceMenu,
+      'menu_img': imgUrl,
+    };
+  }
 }
 
 class DetailMenu {
