2e04cc332aceb8a12f985973607e6f88c61366a4
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-06-13 23:40:55 +0700

    fix detail product view
    non backend

--- lib/app/modules/detail_produk/views/bottom_sheet.dart
+++ lib/app/modules/detail_produk/views/bottom_sheet.dart
@@ -1,61 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:adella_kitchen/theme/color.dart';
+import 'package:input_quantity/input_quantity.dart';
 
 class BottomViewDetailProduk extends StatelessWidget {
-  const BottomViewDetailProduk({Key? key}) : super(key: key);
+  const BottomViewDetailProduk({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
+    return Container(
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 230, 230, 230),
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20), topLeft: Radius.circular(20))),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Expanded(
-            child: ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Item ditambahkan ke keranjang')),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                backgroundColor: myColor().primaryColor,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Jumlah pesanan',
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: InputQty(
+                        decoration: QtyDecorationProps(
+                            isBordered: false,
+                            borderShape: BorderShapeBtn.circle,
+                            btnColor: myColor().primaryColor,
+                            width: 8),
+                        steps: 1,
+                        initVal: 5,
+                        minVal: 1,
+                        onQtyChanged: (value) {}),
+                  ),
+                  const Text(
+                    'Minimal pembelian item 5',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
               ),
-              child: Text(
-                'Tambah Keranjang',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text('Subtotal'),
+                  Text('Rp. 50.000',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: myColor().primaryColor)),
+                ],
+              )
+            ],
           ),
-          SizedBox(width: 16), // Space between buttons
-          Expanded(
-            child: ElevatedButton(
-              onPressed: () {
-                // Add logic for the second button
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                backgroundColor: Colors.green, // Example color for the second button
-              ),
-              child: Text(
-                'Second Button',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+          const SizedBox(
+            height: 13,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: myColor().primaryColor, width: 2.5),
+                          borderRadius: BorderRadius.circular(8))),
+                  onPressed: () {},
+                  child: Text(
+                    'Beli Sekarang',
+                    style: TextStyle(color: myColor().primaryColor),
+                  )),
+              const Spacer(),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    backgroundColor: myColor().primaryColor),
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_cart,
                   color: Colors.white,
                 ),
-              ),
-            ),
+                label: const Text(
+                  'Tambah Keranjang',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
           ),
         ],
       ),
--- lib/app/modules/detail_produk/views/detail_produk_view.dart
+++ lib/app/modules/detail_produk/views/detail_produk_view.dart
@@ -1,4 +1,5 @@
 import 'package:adella_kitchen/app/modules/detail_produk/views/bottom_sheet.dart';
+import 'package:adella_kitchen/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/detail_produk_controller.dart';
@@ -18,116 +19,162 @@ class DetailProdukView extends GetView<DetailProdukController> {
     String? selectedItem1 = items[0];
     String? selectedItem2 = items[0];
 
+    final bottomSheetHeight = MediaQuery.of(context).size.height * 0.2;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detail Produk'),
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Image.asset(
-              '/img/menu1.png',
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              padding: EdgeInsets.only(bottom: bottomSheetHeight + 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          'assets/img/menu1.png',
+                          width: double.infinity,
+                          height: 300,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Positioned(
+                        top: 20,
+                        left: 12,
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            shape: BoxShape.circle,
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.arrow_back,
+                                color: Colors.white),
+                            onPressed: () {
+                              Get.back();
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 10),
+                        Text(
+                          'Pake Catering Ayam',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'Rp. 10.000',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: myColor().primaryColor,
+                              ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          '* Tambahan minuman',
+                          style:
+                              Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                        ),
+                        const SizedBox(height: 8),
+                        // First dropdown button with border
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: DropdownButton<String>(
+                            value: selectedItem1,
+                            onChanged: (String? newValue) {
+                              selectedItem1 = newValue!;
+                            },
+                            items: items
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            isExpanded: true,
+                            underline: const SizedBox(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          '* Tambahan makanan',
+                          style:
+                              Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                        ),
+                        const SizedBox(height: 8),
+                        // Second dropdown button with border
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: DropdownButton<String>(
+                            value: selectedItem2,
+                            onChanged: (String? newValue) {
+                              selectedItem2 = newValue!;
+                            },
+                            items: items
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            isExpanded: true,
+                            underline: const SizedBox(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 16),
-                Text(
-                  'Pake Catering Ayam',
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                  style: Theme.of(context).textTheme.bodyText2,
-                  textAlign: TextAlign.justify,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  'Rp. 10.000',
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  '* Tambahan minuman',
-                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                        fontWeight: FontWeight.normal,
-                      ),
-                ),
-                const SizedBox(height: 8),
-                // First dropdown button with border
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: DropdownButton<String>(
-                    value: selectedItem1,
-                    onChanged: (String? newValue) {
-                      selectedItem1 = newValue!;
-                    },
-                    items: items.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    isExpanded: true,
-                    underline: SizedBox(),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  '* Tambahan makanan',
-                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                        fontWeight: FontWeight.normal,
-                      ),
-                ),
-                const SizedBox(height: 8),
-                // Second dropdown button with border
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: DropdownButton<String>(
-                    value: selectedItem2,
-                    onChanged: (String? newValue) {
-                      selectedItem2 = newValue!;
-                    },
-                    items: items.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    isExpanded: true,
-                    underline: SizedBox(),
-                  ),
-                ),
-                const SizedBox(height: 16),
-              ],
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: BottomViewDetailProduk(),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      bottomSheet: BottomViewDetailProduk(),
     );
   }
 }
